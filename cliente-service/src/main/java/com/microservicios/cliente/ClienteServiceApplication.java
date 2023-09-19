7a16520311c1d31b1459e1957bb77275a9b88fd4
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-09-19 16:03:15 -0300

    eureka

--- cliente-service/src/main/java/com/microservicios/cliente/ClienteServiceApplication.java
+++ cliente-service/src/main/java/com/microservicios/cliente/ClienteServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservicios.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // En versiones actuales, reemplaza a @EnableEurekaClient
 public class ClienteServiceApplication {
 
 	public static void main(String[] args) {
--- producto-service/src/main/java/com/microservicios/producto/ProductoServiceApplication.java
+++ producto-service/src/main/java/com/microservicios/producto/ProductoServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservicios.producto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // En versiones actuales, reemplaza a @EnableEurekaClient
 public class ProductoServiceApplication {
 
 	public static void main(String[] args) {
